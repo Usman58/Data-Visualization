7cc5e7d336c4299cb8d2d3ca4fe82f86b508b49f
Usman <Usman10@corp.ibexglobal.com>
2022-09-22 07:53:10 -0700

    data by Dates

--- Reports/views.py
+++ Reports/views.py
@@ -6,6 +6,7 @@ import plotly.graph_objs as go
 import plotly.express as px
 import pandas as pd
 
+
 #https://www.kaggle.com/code/kanncaa1/plotly-tutorial-for-beginners/notebook
 '''
 import mysql.connector
@@ -24,106 +25,75 @@ mycursor = mydb.cursor()
         print(x)
 
 '''
+import pyodbc 
+
+con = pyodbc.connect('Driver={SQL Server};'
+                      'Server=LHR-TCH-USMAN;'
+                      'Database=SocialCRM_JAZZ_V3;'
+                      'Trusted_Connection=yes;')
+
+def connect(query):
+    cursor = con.cursor()
+    print('Connection to db successful')
+    #cmd = (query)
+    #results = cursor.execute(cmd).fetchall()
+    df = pd.read_sql(query, con)
+    return df
+
+
 
 def home(request):
-    dataset=pd.read_csv("static\R_U_Dataset.csv")
-    datacolumns=dataset.columns
-    print("datacolumns",datacolumns)
+    df=connect('SELECT * FROM PostsTwitter')
+    total_posts=df.shape[0]
+    print("total_posts",total_posts)
+    datacolumns=df.columns
     if request.method=="POST":
-           data=request.POST.getlist('listcolumns')
-           for column in data:
-                      if column=="sentiment":
-                                 counts=dataset[column].value_counts()
-           Pos=counts['Positive']
-           Neg=counts['Negative']
-           print("Neg",Neg)
-           Neu=counts['Neutral']
-           x=['Positive','Negative','Neutral']
-           y=[Pos,Neg,Neu]
-           fig = go.Bar(x=x, y=y,name="Sentiment Analysis",marker=dict(color='green'))
-           layout1 = {
-               'title': 'Sentiment Analysis',
-                'xaxis_title': 'labels',
-                'yaxis_title': 'values',
-                'height': 420,
-             'width': 560,
-           }
-           bar_div=plot({'data':fig,'layout': layout1},output_type='div')
-           return render(request, 'admin.html',context={'bar_div':bar_div})
-    """ 
-    View demonstrating how to display a graph object
-    on a web page with Plotly. 
- 
-    data=pd.read_csv("static\R_U_Dataset.csv")
-    datacolumns=data.columns
-    print("datacolumns",datacolumns)
-    #df=pd.DataFrame(df)
-    """
-    
-        #df=pd.DataFrame(data)
-    #fig = px.bar(x=Positive, y=Negative,z=Neutral)
-    # Generating some data for plots.
-    x = [i for i in range(0, 11)]
-    #print("x",x)
-    y1 = [3*i for i in x]
-    #print("y1",y1)
-    y2 = [i**2 for i in x]
-    #print("y2",y2)
-    y3 = [10*i for i in x]
-    #print("y3",y3)
-
-    # List of graph objects for figure.
-    # Each object will contain on series of data.
-    graphs = []
-
-    # Adding linear plot of y1 vs. x.
-    graphs.append(
-        go.Scatter(x=x, y=y1, mode='lines', name='Line y1')
-    )
-
-    # Adding scatter plot of y2 vs. x. 
-    # Size of markers defined by y2 value.
-    graphs.append(
-        go.Scatter(x=x, y=y2, mode='markers', opacity=0.8, 
-                   marker_size=y2, name='Scatter y2')
-    )
-
-    # Adding bar plot of y3 vs x.
-    graphs.append(
-        go.Bar(x=x, y=y3, name='Bar y3')
-    )
-
-    # Setting layout of the figure.
+        From=request.POST['startDate']
+        To=request.POST['endDate']
+        #    From = "2022-01-17"
+        #    To = "2022-01-25"
+        print(From,To)
+        query = f"SELECT * FROM PostsTwitter where CreatedDate >= '{From}' AND CreatedDate <= '{To}'"
+        print(query)
+        df=connect(query)
+        print(df)
+        #df = df[(df['CreatedDate'] >= From) & (df['CreatedDate'] <= To)]
+        print("df",df.shape[0])
+        
+    #    2022-01-17 11:49:21.000
+    #    2022-01-25 18:23:00.000
+    #    for column in data:
+    #             if column=="sentiment"
+    #                 counts=dataset[column].value_counts()
+    #    Pos=counts['Positive']
+    #    Neg=counts['Negative']
+    #    Neu=counts['Neutral']
+        x=['Created Date']
+        y=[df.shape[0]]
+        fig1 = go.Bar(x=x, y=y,name="Sentiment Analysis",marker_color=['green'])
+        #fig2 = go.Pie(labels=x, values=y,name="Sentiment Analysis",marker=dict(colors=['green']))
+        #fig2 = px.pie(values=y, names=x)
+        layout = {
+            'title': 'Data From Selected Dates',
+            'xaxis_title': 'Dates',
+            'yaxis_title': 'values',
+            'height': 400,
+            'width': 500,
+        }
+        bar_div=plot({'data':fig1,'layout': layout},output_type='div')
+        #pie_div=plot({'data':fig2,'layout': layout},output_type='div')
+        
+        context={'bar_div':bar_div}
+        return render(request, 'admin.html',context)
+    x=['Total Posts']
+    y=[total_posts]
     layout = {
-        'title': 'Title of the figure',
-        'xaxis_title': 'X',
-        'yaxis_title': 'Y',
-        'height': 420,
-        'width': 560,
-    }
-
-
-    # Getting HTML needed to render the plot.
-    plot_div = plot({'data': graphs, 'layout': layout}, 
-                    output_type='div')
-
- 
-    return render(request, 'admin.html',context={'plot_div':plot_div,'datacolumns':datacolumns})
-
-
-
-def Graph(request):
-    data=request.POST.getlist('listcolumns')
-    dataset=pd.read_csv("static\R_U_Dataset.csv")
-    for column in data:
-        if column=="sentiment":
-            counts=dataset[column].value_counts()
-    counts=Pos=counts['Positive']
-
-    Neg=counts['Negative']
-    print("Neg",Neg)
-    Neu=counts['Neutral']
-    x=['Positive','Negative','Neutral']
-    y=[Pos,Neg,Neu]
-    
-    return render(request, 'base.html',context={})
+               'title': 'Tweeter Posts Count',
+                'xaxis_title': 'Posts',
+                'yaxis_title': 'count',
+                'height': 400,
+                'width': 500,
+           }
+    fig = go.Bar(x=x, y=y,name="Tweeter Posts Count",marker_color=['green'])
+    Tweets_div=plot({'data':fig,'layout': layout},output_type='div')
+    return render(request, 'admin.html',context={'datacolumns':datacolumns,'Tweets_div':Tweets_div})
--- ReportsProject/urls.py
+++ ReportsProject/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
-    path('graph/',views.Graph,name="graph"),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
